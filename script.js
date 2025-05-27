8bd80bdb26678d3fa5b3b7366bbebc1cc53cae08
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-27 17:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,28 +11,59 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartlist = document.getElementById('cart-list');
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" value="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	const sessionData ={};
+  for(let i=0;i<sessionStorage.length;i++){
+	  const key  = sessionStorage.key(i);
+	  sessionData[key] = JSON.parse(sessionStorage.getItem(key));
+	  
+  }
+	sessionData.map((data,index)=>{
+		const li =document.createElement('li');
+		li.innerHTML = `${data.name} - ${data.price} <button class="btn" value=`${index}`>Remove Item</button>`;
+		cartlist.appendChild(li);
+	})
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	let obj = [{"name":`Product ${productId}`},{"price":`$${productId*10}`}];
+	sessionStorage.setItem(`${productId}`,JSON.Stringify(obj));
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	sessionStorage.removeItem("productId");
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	sessionStorage.clear();
+}
 
 // Initial render
 renderProducts();
 renderCart();
+
+document.getElementById('clear-cart-btn').addEventListener("click",()=>{
+	clearCart();
+})
+document.getElementsByclassName('btn').addEventListener("click",(e)=>{
+	removeFromCart(e.target);
+})
+document.getElementsByclassName('add-to-cart-btn').addEventListener("click",(e)=>{
+	addToCart(e.target);
+})
+
